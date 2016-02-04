976ca30127f8b19d03b794880d55f3d3ef0e935a
NBAS\brschoen <NBAS\brschoen>
2016-02-04 09:38:50 -0600

    Stubbed out aurora shipment job

---
+++ Source/WmMiddleware/Middleware.Wm.Aurora.Shipment/AuroraShipmentJob.cs
@@ -0,0 +1,20 @@
+﻿using Middleware.Jobs;
+using MiddleWare.Log;
+
+namespace Middleware.Wm.Aurora.Shipment
+{
+    public class AuroraShipmentJob : IUnitOfWork
+    {
+        private readonly ILog _log;
+
+        public AuroraShipmentJob(ILog log)
+        {
+            _log = log;
+        }
+
+        public void RunUnitOfWork(string jobKey)
+        {
+            _log.Info("Hello");
+        }
+    }
+}
---
+++ Source/WmMiddleware/Middleware.Wm.Aurora.Shipment/DependencyInjection/NinjectModuleConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Middleware.Jobs;
+using Middleware.Jobs.Repositories;
+using MiddleWare.Log;
+using Ninject.Modules;
+using WmMiddleware.Configuration;
+using WmMiddleware.TransferControl.Control;
+using WmMiddleware.TransferControl.Ftp;
+using WmMiddleware.TransferControl.Repositories;
+
+namespace Middleware.Wm.Aurora.Shipment.DependencyInjection
+{
+    public class NinjectModuleConfiguration : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILog>().To<Log4Net>();
+            Bind<IJobRepository>().To<JobRepository>();
+            Bind<IConfigurationManager>().To<MiddlewareConfigurationManager>();
+            Bind<IUnitOfWork>().To<AuroraShipmentJob>();
+            Bind<IFileIo>().To<FileIo>();
+            Bind<ITransferControlRepository>().To<TransferControlRepository>();
+            Bind<IFtpClientFactory>().To<SftpClientFactory>();
+          // TODO  Bind<IFtpClientConfiguration>().To<AuroraFtpClientConfiguration>();
+        }
+    }
+}
+       
