8484ad3d01d2c070348882b14afaf0ac3b0acf70
NBAS\brschoen <NBAS\brschoen>
2016-03-18 16:10:14 -0500

    bump version

--- Source/WmMiddleware/GlobalAssemblyInfo.cs
+++ Source/WmMiddleware/GlobalAssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("0.9.0.9")]
-[assembly: AssemblyFileVersion("0.9.0.9")]
+[assembly: AssemblyVersion("0.9.0.10")]
+[assembly: AssemblyFileVersion("0.9.0.10")]
 [assembly: ComVisible(false)]
