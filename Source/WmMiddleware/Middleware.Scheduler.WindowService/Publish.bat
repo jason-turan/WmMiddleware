16703ca283390524b49b8e233b51f10a251c4ca2
jason_turan <jason.turan@newbalance.com>
2016-04-14 12:36:21 -0500

    Minor updates

--- Source/WmMiddleware/Middleware.Scheduler.WindowService/Publish.bat
+++ Source/WmMiddleware/Middleware.Scheduler.WindowService/Publish.bat
@@ -3,7 +3,7 @@ sc \\STLMW01 stop "New Balance Warehouse Management Middleware"
 
 timeout 10
 
-xcopy /s /y * \\Stlmw01\d$\WmMiddleware\Scheduler
+xcopy /s /y Middleware.Scheduler.WindowService\* \\Stlmw01\d$\WmMiddleware\Scheduler
 
 sc \\STLMW01 start "New Balance Warehouse Management Middleware"
 
