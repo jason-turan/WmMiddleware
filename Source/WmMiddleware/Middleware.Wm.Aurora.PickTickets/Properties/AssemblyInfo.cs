79901ec4181e728269cb61f9903021940031f35e
jason_turan <jason.turan@newbalance.com>
2016-04-15 09:16:32 -0500

    Updating version for assemblies.

--- Source/WmMiddleware/Middleware.Wm.Aurora.PickTickets/Properties/AssemblyInfo.cs
+++ Source/WmMiddleware/Middleware.Wm.Aurora.PickTickets/Properties/AssemblyInfo.cs
@@ -6,11 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Middleware.WarehouseManagement.Aurora.PickTickets")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")] 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Middleware.WarehouseManagement.Aurora.PickTickets")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- Source/WmMiddleware/Middleware.Wm.LoggingAndFileMaintenance/Properties/AssemblyInfo.cs
+++ Source/WmMiddleware/Middleware.Wm.LoggingAndFileMaintenance/Properties/AssemblyInfo.cs
@@ -6,11 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Middleware.Wm.LoggingAndFileMaintenance")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")] 
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Middleware.Wm.LoggingAndFileMaintenance")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Middleware.Wm.LoggingAndFileMaintenance")] 
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +30,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+ 
