1a1bf5a088a9e34066b9fe0d8935a49809a7296e
jason_turan <jason.turan@newbalance.com>
2016-04-27 14:41:18 -0500

    Adding database restore script

---
+++ Source/WmMiddleware/Middleware.Wm.Service.Inventory.WebJob/PoisonQueueMonitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.WebJobs;
+
+namespace Middleware.Wm.Service.Inventory.WebJob
+{
+    class PoisonQueueMonitor
+    {
+
+        public void StartupJob(
+[TimerTrigger("0 0 */2 * * *", RunOnStartup = true)] TimerInfo timerInfo)
+        {
+            Console.WriteLine("Timer job fired!");
+        }
+    }
+}
